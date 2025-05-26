0511e77a3c89f8a601cdf6b41c71e17ba8428303
adam-liao <adam.liao@gmail.com>
2025-05-26 15:44:58 +0800

    update

--- lesson6/index.py
+++ lesson6/index.py
@@ -1,25 +1,24 @@
 from flask import Flask,render_template
-# 要載入 這二個套件
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html.jinja2")
-# 這裡要指向首頁檔案的名稱
 
 @app.route("/classes")
 def classes():
     return render_template("classes.html.jinja2")
 
-@app.route("/contact")
-def contact():
-     return render_template("contact.html.jinja2")
+@app.route("/new")
+def new():
+    return render_template("new.html.jinja2")
 
 @app.route("/traffic")
 def traffic():
-     return render_template("traffic.html.jinja2")
+    return render_template("traffic.html.jinja2")
 
-@app.route("/news")
-def news():
-     return render_template("news.html.jinja2")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html.jinja2")
+
