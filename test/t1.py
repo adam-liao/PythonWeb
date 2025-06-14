b4afe4c047272378a86a94cba5a27d30761c6d5a
adam-liao <adam.liao@gmail.com>
2025-06-14 12:51:59 +0800

    ok

--- test/t1.py
+++ test/t1.py
@@ -1,5 +1,12 @@
 import jieba
 
-text = "今天心情很低落，什麼都提不起勁"
-words = jieba.lcut(text)
-print(words)
+# text = "今天心情很低落，什麼都提不起勁"
+
+while True:
+    text = input("請輸入一段文字 (輸入 'exit' 或 'quit' 結束): ")
+    if text.lower() == 'exit' or text.lower() == 'quit':
+        break
+    words = jieba.lcut(text)
+    print(words)
+
+print("程式已結束")
